c0ccb091c8203f1e7ae0e15d2874639825f83095
au-lex <aulexc4d2@gmail.com>
2024-01-27 13:43:10 +0100

    still responsive

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -114,7 +114,7 @@ const Header = ({ chartData }) => {
         </div>
 
         <div className="sales flex flex-wrap rounded-[10px] items-center  pt-[2rem]  lg:w-[50%] w-full lg:h-[50vh]">
-    <div className="total-order lg:w-[47%] w-[45%] border shadow-sm h-[25vh] m-2 rounded-[10px]">
+    <div className="total-order lg:w-[47%] w-[45%] border shadow-sm h-[28vh] m-2 rounded-[10px]">
 
     <div className="no w-[100%] flex justify-between px-[1rem] pt-3   h-[3rem]  rounded-full  py-2">
     <div className=" w-[2rem] h-[2rem]
@@ -131,8 +131,8 @@ const Header = ({ chartData }) => {
  <h2 className='font-semibold text-[25px]'>350</h2>
  </section>
 
-        <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-4">
-        <span className='flex px-2 space-x-2 bg-green-200   rounded-full '>
+        <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-2">
+        <span className=' px-2 space-x-1 bg-green-200 h-[3rem] w-[3rem] rounded-full '>
    
     <div className="pt-2  w-[100%] rounded-full">
        
@@ -147,7 +147,7 @@ const Header = ({ chartData }) => {
 </section>
     </div>
 
-    <div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[25vh] m-2 rounded-[10px]">
+    <div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[28vh] m-2 rounded-[10px]">
 
 <div className="no w-[100%] flex justify-between px-[1rem] pt-3   h-[3rem]  rounded-full  py-2">
 <div className=" w-[2rem] h-[2rem]
@@ -165,8 +165,8 @@ const Header = ({ chartData }) => {
 <h2 className='font-semibold text-[25px]'>270</h2>
 </section>
 
-    <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-4">
-    <span className='flex px-2 space-x-2 bg-red-200   rounded-full '>
+    <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-2">
+    <span className=' px-2 space-x-2 bg-red-200  h-[3rem] w-[3rem]  rounded-full '>
 
 <div className="pt-2  w-[100%] rounded-full">
    
@@ -181,7 +181,7 @@ const Header = ({ chartData }) => {
 </section>
 </div>
 
-<div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[25vh] m-2 rounded-[10px]">
+<div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[28vh] m-2 rounded-[10px]">
 
 <div className="no w-[100%] flex justify-between px-[1rem] pt-3   h-[3rem]  rounded-full  py-2">
 <div className=" w-[2rem] h-[2rem]
@@ -198,8 +198,8 @@ const Header = ({ chartData }) => {
 <h2 className='font-semibold text-[25px]'>1,567</h2>
 </section>
 
-    <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-4">
-    <span className='flex px-2 space-x-2 bg-red-200   rounded-full '>
+    <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-2">
+    <span className=' px-2 space-x-2 bg-red-200  h-[3rem] w-[3rem]  rounded-full '>
 
 <div className="pt-2  w-[100%] rounded-full">
    
@@ -214,7 +214,7 @@ const Header = ({ chartData }) => {
 </section>
 </div>
 
-<div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[25vh] m-2 rounded-[10px]">
+<div className="total-order lg:w-[47%]  w-[45%] border shadow-sm h-[28vh] m-2 rounded-[10px]">
 
     <div className="no w-[100%] flex justify-between px-[1rem] pt-3   h-[3rem]  rounded-full  py-2">
     <div className=" w-[2rem] h-[2rem]
@@ -232,16 +232,16 @@ const Header = ({ chartData }) => {
  </section>
 
         <section className="desc flex space-x-4 justify-betwee px-[1rem] pt-4">
-        <span className='flex px-2 space-x-2 bg-green-200   rounded-full '>
+        <span className=' px-2 space-x-1 bg-green-200 h-[3rem] w-[3rem] rounded-full '>
    
-    <div className="pt-2  w-[100%] rounded-full">
-       
-      <img src={group} alt="" />
+   <div className="pt-2  w-[100%] rounded-full">
+      
+     <img src={group} alt="" />
 
-     
-        </div>
-        <h2 className='text-[12px] text-green-800 '>23.5%</h2>                                   
-  </span>
+    
+       </div>
+       <h2 className='text-[12px] text-green-800 '>23.5%</h2>                                   
+ </span>
   <h1 className='capitalize text-[15px]  '>vs.previous month</h1>
   
 </section>
@@ -249,6 +249,219 @@ const Header = ({ chartData }) => {
 
 </div>
 
+  <div className=''>
+
+  <div className="user px-[1rem]">
+
+  
+  <div class="overflow-x-auto  mt-[2rem]">
+  <table className = 'min-w-full'>
+  <tr>
+    <th className='text-slate-700  text-[15px]  font-light'>Name</th>
+    <th className='text-slate-700 text-[15px]   font-light' >Date</th>
+    <th className='text-slate-700 text-[15px]  font-light'>Amount</th>
+    <th className='text-slate-700 text-[15px]   font-light'>Status</th>
+    <th className='text-slate-700  text-[15px] font-light'>Invoice</th>
+  </tr>
+  <tr>
+    <td className='flex space-x-2'>
+    <span className='w-[40px] h-[40px]'><img className='w-full' src={profile} alt="" /></span>
+      <h1 className='   font-semibold text-slate-800 text-[15px]'>  Marcos Bergson</h1>
+    </td>
+    <td  className='text-[10px] w-full'>Nov 15, 2023</td>
+    <td className='   text-[14px] font-semibold text-slate-800'>$80,000</td>
+    <td  className='text-[14px]'>Paid</td>
+    <td className='flex space-x-1 '>
+    <span >
+
+    <CiFileOn />
+    </span>
+      <h1 className='   text-[14px]'>Veiw</h1>
+    </td>
+
+  </tr>
+  <tr>
+  <td className='flex space-x-2'>
+  <span className='w-[40px] h-[40px]'><img className='w-full' src={profile} alt="" /></span>
+      <h1 className='   font-semibold text-slate-800 text-[15px]'> Jaydon Vaccro</h1>
+    </td>
+    
+    <td  className='text-[10px]'>Nov 15, 2023</td>
+    <td className='   text-[14px] font-semibold text-slate-800'>$80,000</td>
+    <td  className='text-[14px]'>Refund</td>
+    <td className='flex space-x-1 '>
+    <span >
+
+    <CiFileOn />
+    </span>
+      <h1 className='   text-[14px]'>Veiw</h1>
+    </td>
+  </tr>
+  <tr>
+  <td className='flex space-x-2'>
+  <span className='w-[40px] h-[40px]'><img className='w-full' src={profile} alt="" /></span>
+      <h1 className='  font-semibold text-slate-800 text-[15px]'>Corey Schleifer</h1>
+    </td>
+   
+    <td  className='text-[10px]'>Nov 15, 2023</td>
+    <td className='   text-[14px] font-semibold text-slate-800'>$80,000</td>
+    <td  className='text-[14px]'>Paid</td>
+    <td className='flex space-x-1 '>
+    <span >
+
+    <CiFileOn />
+    </span>
+      <h1 className='   text-[14px]'>Veiw</h1>
+    </td>
+  </tr>
+  <tr>
+  <td className='flex space-x-2'>
+      <span className='w-[40px] h-[40px]'><img className='w-full' src={profile} alt="" /></span>
+      <h1 className='  font-semibold text-slate-800 text-[15px]'> Cooper press</h1>
+    </td>
+   
+    <td  className='text-[10px]'>Nov 15, 2023</td>
+    <td className='   text-[14px] font-semibold text-slate-800'>$80,000</td>
+    <td  className='text-[14px]'>Refund</td>
+    <td className='flex space-x-1 '>
+    <span >
+
+    <CiFileOn />
+    </span>
+      <h1 className='   text-[14px]'>Veiw</h1>
+    </td>
+  </tr>
+  <tr>
+  <td className='flex space-x-2'>
+  <span className='w-[40px] h-[40px]'><img className='w-full' src={profile} alt="" /></span>
+      <h1 className='  font-semibold text-slate-800 text-[15px]'> Phlip rubin</h1>
+    </td>
+    
+    <td  className='text-[10px]'>Nov 15, 2023</td>
+    <td className='   text-[14px] font-semibold text-slate-800'>$80,000</td>
+    <td className='text-[14px]'>Paid</td>
+    <td className='flex space-x-1 '>
+    <span >
+
+    <CiFileOn />
+    </span>
+      <h1 className='   text-[14px]'>Veiw</h1>
+    </td>
+  </tr>
+  
+</table>
+</div>
+</div>
+<section className="tabC px-[.5rem]">
+
+
+<div className="sales flex flex-wrap  rounded-[10px] mt-[2rem]  items-center w-[100%]  h-[50vh]">
+    <div className="total-order w-[100%] border shadow-sm px-[1rem]  mt-2  rounded-[10px]">
+
+    <section className="desc flex justify-between  pt-4 mb-4">
+  <h1 className='capitalize  font-semibold'>TopPlatform </h1>
+  <span className='flex '>
+    <h2 className='text-green-400 font-semibold'>See All</h2>
+   
+  </span>
+
+ 
+</section>
+
+
+<div>
+
+
+
+
+
+
+<span>
+ <label className='block font-semibold' htmlFor="">Book Bazaar</label>
+<meter className='w-full h-[1.5rem]' id="diskE" value="6" min="0" max="10" low="9"></meter>
+<section className="desc flex justify-between pt-2">
+  <h1 className='capitalize  '>$2,500 000 </h1>
+  <span className='flex '>
+    <h2 className=''>+15%</h2>
+   
+  </span>
+  
+ 
+</section>
+
+</span>
+
+
+<span>
+ <label className='block font-semibold' htmlFor="">Artisian Asile</label>
+<meter className='w-full h-[1.5rem] ' id="diskA" value="5" min="0" max="10" low="9"></meter>
+<section className="desc flex justify-between pt-2">
+  <h1 className='capitalize  '>$1,800 000 </h1>
+  <span className='flex '>
+    <h2 className=''>+13%</h2>
+   
+  </span>
+  
+ 
+</section>
+
+</span>
+
+
+<span>
+ <label className='block font-semibold' htmlFor="">Toy Troop</label>
+<meter className='w-full h-[1.5rem]' id="diskB" value="4" min="0" max="10" low="9"></meter>
+<section className="desc flex justify-between pt-2">
+  <h1 className='capitalize  '>$1,200 000 </h1>
+  <span className='flex '>
+    <h2 className=''>+12%</h2>
+   
+  </span>
+  
+ 
+</section>
+
+</span>
+
+
+<span>
+ <label className='block font-semibold' htmlFor="">Xstore</label>
+<meter className='w-full h-[1.5rem]' id="diskC" value="3" min="0" max="10" low="9"></meter>
+<section className="desc flex justify-between pt-2">
+  <h1 className='capitalize  '>$1,000 000 </h1>
+  <span className='flex '>
+    <h2 className=''>+10%</h2>
+   
+  </span>
+  
+ 
+</section>
+
+</span>
+
+<span>
+ <label className='block font-semibold' htmlFor="">Book Bazaar</label>
+<meter className='w-full h-[1.5rem]' id="diskD" value="2" min="0" max="10" low="9"></meter>
+<section className="desc flex justify-between pt-2">
+  <h1 className='capitalize  '>$800 000 </h1>
+  <span className='flex '>
+    <h2 className=''>+8%</h2>
+   
+  </span>
+  
+ 
+</section>
+
+</span>
+
+
+
+</div>
+    </div>
+    
+</div>
+</section>
+</div>
 </section>
 
 
@@ -307,30 +520,6 @@ const Header = ({ chartData }) => {
     </div>
 
 
- <div className="sales border shadow-sm w-full lg:w-[60%] rounded-[10px] h-[52vh] mt-2">
-<section className="desc flex justify-between px-[1rem] pt-1">
-  <h1 className='capitalize  font-semibold'>sales trend</h1>
-  <span className='flex '>
-    <h2 className='mr-2'>Shotby:</h2>
-    <div className=" flex   space-x-4 shadow-sm border  px-2 py-1 w-[60%] rounded-full">
-       
-      <div className="">
-        <h2 className='text-[12px]  '>Weekly</h2>
-        
-      </div>
-      <div className="pt-1">
-
-      <FaChevronDown className='text-slate-600 text-[12px]'  />
-      </div>
-        </div>
-  </span> 
-</section>
-
-<div className='  w-full flex px-[1rem] justify-center'>
-
-<Chart chartData={chartData} />
-</div>
-        </div>
 {/* herro */}
     <section className="hero flex">
 
@@ -559,7 +748,7 @@ const Header = ({ chartData }) => {
 
 
         
-       <div className='dash-Container flex-wrap flex  w-full justify-between lg:space-x-6 mb-[2rem]'>
+       <div className='dash-Container  flex  w-full justify-between lg:space-x-6 mb-[2rem]'>
 
         <div className="sales border shadow-sm  b-red-500 lg:w-[60%] w-full  rounded-[10px] lg:h-[70vh] mt-2">
 <section className="desc flex justify-between px-[1.5rem] pt-4">
